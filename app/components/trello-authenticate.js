9274341ebacf3d049120447d18ead290305b292c
Rohan <rohanpujaris@gmail.com>
2016-02-04 00:51:38 +0530

    first commit

---
+++ app/components/trello-authenticate.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    authenticate: function() {
+      Trello.authorize({
+        type: "popup",
+        name: "Trello stats",
+        scope: { read: "allowRead", write: "allowWrite", account: "allowAccount" },
+        expiration: 'never',
+        success: function() {
+          this.sendAction('trelloAuthSuccess', localStorage.getItem('trello_token'));
+        }.bind(this),
+        error: function() {
+
+        }
+      });
+    }
+  }
+});
