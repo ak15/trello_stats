3fb138f1fe4bb7e604c7e0592c79130531fc898d
Rohan Pujari <rohan.pujari@kiprosh.com>
2016-03-07 16:21:20 +0530

    Fix issue while pulling data from trello. "Unknown method 'request' for undefined"

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isAuthenticated: Ember.computed.oneWay('authentication.isAuthenticated'),
+  ajax: Ember.inject.service(),
   actions: {
     pullAllData() {
       return this.get('ajax').request('/pull_all_data_from_trello');
     }
   }
-})
+});
